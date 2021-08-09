ab7b25f0cd286d307eee6f7d8c802e9b5d142ea4
iamoperand <narora200@gmail.com>
2021-08-10 00:51:54 +0530

    feat: add deploy script

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,8 +1,18 @@
+require('dotenv').config()
 require('@nomiclabs/hardhat-waffle')
+
+
+const {ALCHEMY_API_KEY, ROPSTEN_PRIVATE_KEY} = process.env
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
 };
